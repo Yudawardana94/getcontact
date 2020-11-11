877ef1ab438f331e6e46a52b51eb8421924e3422
Yudawardana94 <yuda.wardana40@gmail.com>
2020-11-11 20:22:04 +0700

    [INIT] first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View >
+      <Text>Application: getcontact</Text>
+    </View>
+  )
+}
+
+export default App
